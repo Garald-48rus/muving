7cf744ec2c6017fef3620844844ea613d3070d61
Garald-48rus <56934346+Garald-48rus@users.noreply.github.com>
2020-06-21 19:30:13 +0300

    Create primaryfield.cpp

---
+++ primaryfield.cpp
@@ -0,0 +1,16 @@
+#include "primaryfield.h"
+
+primaryField::primaryField()
+    :field(3, 4, 70)
+{
+    init_cells();
+}
+//------------------------------------------------------------------------------
+void primaryField::init_cells()
+{
+  mvFields.resize(mLines*mColumns);
+
+  for(int i = 0; i < mLines*mColumns; ++i)
+    mvFields[i] = eCellType::EMPTY;
+
+}
